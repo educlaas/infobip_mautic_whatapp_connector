f5105e5097a9ab12df6a8024a0f5b497c14fd510
educlaas <120721228+educlaas@users.noreply.github.com>
2022-12-19 13:30:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { Infobip, AuthType } from "@infobip-api/sdk";
 
 let infobip = new Infobip({
   baseUrl: "https://jdv2pv.api.infobip.com",
-  apiKey: "77e0a4abfb399978a5f0f879f87055ff-3e64c26d-724c-4f84-831f-fb2b9f1e6a44",
+  apiKey: "",
   authType: AuthType.ApiKey,
 });
 
